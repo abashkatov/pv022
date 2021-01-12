217397489d2211dd1b65b3024d3a84e047af7dec
abashkatov <as@bashkatov.pro>
2021-01-12 21:40:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BackgroundColor = ConsoleColor.Gray;
+            string hello = "Hello"; // 6
+            /*
+             *          3   5
+             *      * * * * * * * * * *
+             *      * * H e l l o   * *
+             *      * * * * * * * * * *
+             * 
+             * */
+            //int rows = LargestWindowHeight;
+            //int cols = LargestWindowWidth;
+            int rowMid = WindowHeight / 2;
+            int colMid = WindowWidth / 2;
+            int curColPos = colMid - 3;
+            SetCursorPosition(curColPos, rowMid);
+            WriteLine(hello);
+
+            //Clear();
+
+            ReadKey();
+        }
+    }
+}

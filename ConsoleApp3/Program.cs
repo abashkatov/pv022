c3336a1b18ded457e7ada610ed5e6476235b2a7f
abashkatov <as@bashkatov.pro>
2021-01-14 21:50:18 +0300

    Ветвление и циклы

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,6 +7,69 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            string[] names = new string[3];
+
+            names[0] = "Name1";
+            names[1] = "Name2";
+            names[2] = "Name3";
+            
+            foreach (string name in names)
+            {
+                WriteLine("Name: " + name);
+            }
+
+            int j = 0;
+            int i;
+            for (i = 0; i < 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                if (i > 10)
+                {
+                    WriteLine(i);
+                    break;
+                }
+                WriteLine(i);
+            }
+            WriteLine(i);
+
+
+            i = 0;
+
+            string ans;
+
+            //Вариант 1
+            WriteLine("Вам уже есть 18? (y/n)");
+            ans = ReadLine();
+
+            while (ans != "y" && ans != "n")
+            {
+                WriteLine("Ошибка ввода");
+                WriteLine("Вам уже есть 18? (y/n)");
+                ans = ReadLine();
+            }
+
+            // Вариант 2
+            do
+            {
+                WriteLine("Вам уже есть 18? (y/n)");
+                ans = ReadLine();
+            } while (ans != "y" && ans != "n");
+
+        Label2:
+            WriteLine("Вам уже есть 18? (y/n)");
+            ans = ReadLine();
+            if (ans != "y" && ans != "n")
+            {
+                goto Label2;
+            }
+
+
+            ReadKey();
+            return;
+            // Консоль
             BackgroundColor = ConsoleColor.Gray;
             string hello = "Hello"; // 6
             /*

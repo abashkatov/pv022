3b848bd9bbbc77cfc9d0cab65605258e1fa67a90
abashkatov <as@bashkatov.pro>
2021-01-15 19:14:29 +0300

    Пример создания и объединения массивов

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,87 +7,29 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            string[] names = new string[3];
+            string[] 
+                names1 = { "Name1" , "Name2", "Name3", "NameN" },
+                names2 = { "Name4" , "Name5", "Name6" },
+                namesTotal = new string[2]
+            ;
 
-            names[0] = "Name1";
-            names[1] = "Name2";
-            names[2] = "Name3";
-            
-            foreach (string name in names)
+            namesTotal = new string[names1.Length + names2.Length];
+            int currentPosition = 0;
+            foreach (string name in names1)
             {
-                WriteLine("Name: " + name);
+                namesTotal[currentPosition] = names1[currentPosition];
+                currentPosition++;
+            }
+            foreach (string name in names2)
+            {
+                namesTotal[currentPosition] = names2[currentPosition - names1.Length];
+                currentPosition++;
+            }
+            foreach (string name in namesTotal)
+            {
+                WriteLine(name);
             }
 
-            int j = 0;
-            int i;
-            for (i = 0; i < 20; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    continue;
-                }
-                if (i > 10)
-                {
-                    WriteLine(i);
-                    break;
-                }
-                WriteLine(i);
-            }
-            WriteLine(i);
-
-
-            i = 0;
-
-            string ans;
-
-            //Вариант 1
-            WriteLine("Вам уже есть 18? (y/n)");
-            ans = ReadLine();
-
-            while (ans != "y" && ans != "n")
-            {
-                WriteLine("Ошибка ввода");
-                WriteLine("Вам уже есть 18? (y/n)");
-                ans = ReadLine();
-            }
-
-            // Вариант 2
-            do
-            {
-                WriteLine("Вам уже есть 18? (y/n)");
-                ans = ReadLine();
-            } while (ans != "y" && ans != "n");
-
-        Label2:
-            WriteLine("Вам уже есть 18? (y/n)");
-            ans = ReadLine();
-            if (ans != "y" && ans != "n")
-            {
-                goto Label2;
-            }
-
-
-            ReadKey();
-            return;
-            // Консоль
-            BackgroundColor = ConsoleColor.Gray;
-            string hello = "Hello"; // 6
-            /*
-             *          3   5
-             *      * * * * * * * * * *
-             *      * * H e l l o   * *
-             *      * * * * * * * * * *
-             * 
-             * */
-            //int rows = LargestWindowHeight;
-            //int cols = LargestWindowWidth;
-            int rowMid = WindowHeight / 2;
-            int colMid = WindowWidth / 2;
-            int curColPos = colMid - 3;
-            SetCursorPosition(curColPos, rowMid);
-            WriteLine(hello);
-
-            //Clear();
 
             ReadKey();
         }

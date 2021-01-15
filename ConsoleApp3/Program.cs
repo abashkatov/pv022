313a5e3c8cce168e161c2826d095f64f5d4d3aa5
abashkatov <as@bashkatov.pro>
2021-01-15 19:34:58 +0300

    Забчатые массисы или массив массивов

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,19 +7,22 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            int[,] arr = { { 1,2,3}, { 4,5,6} };
-            //int[,] arr = new int[2, 3] { { 1,2,3}, { 4,5,6} };
-            //arr[0, 0] = 1;
-            //arr[0, 1] = 2;
-            //arr[0, 2] = 3;
-            //arr[1, 0] = 4;
-            //arr[1, 1] = 5;
-            //arr[1, 2] = 6;
+            int[][] arr = new int[3][];
 
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 3; j++)
+            arr[0] = new int[3];
+            arr[0][0] = 1;
+            arr[0][1] = 2;
+            arr[0][2] = 3;
+
+            arr[1] = new int[5] { 4, 5, 6, 7, 8 };
+
+            arr[2] = new int[] { 4, 5, 6, 9 };
+
+
+            for (int i = 0; i < arr.Length; i++) {
+                for (int j = 0; j < arr[i].Length; j++)
                 {
-                    Write(arr[i, j] + "   ");
+                    Write(arr[i][j] + "   ");
                 }
                 WriteLine();
             }

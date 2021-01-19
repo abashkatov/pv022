0af1b8d8e08ea9d6b1ba52d71c67f4018e1ca4d4
abashkatov <as@bashkatov.pro>
2021-01-19 21:19:03 +0300

    Домашнее задание на 21.01.21

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,40 +4,37 @@ using static System.Console;
 
 namespace ConsoleApp3
 {
-    enum Days:short { Monday = 0, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
-    enum Days1 { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
-    enum Days2 { Monday, Tuesday=5, Wednesday, Thursday=15, Friday, Saturday, Sunday}
+    enum РабочиеДни { Понедельник, Вторник, Среда, Четверг, Пятница}
     class Program
     {
         static void Main(string[] args)
         {
-            //string currentDay = "Monday";
-
-            //string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sturday", "Sunday" };
-            //short currentDay = 1;
-
-            Random rnd = new Random();
-            Array enumValues = Enum.GetValues(typeof(Days));
-
-            foreach (Object day in enumValues) {
-                WriteLine((Days)day + " = " + (short)day);
-            }
-
-            string currentDay = "Monday";
-
-            WriteLine(Days.Wednesday + " = " + (short)Days.Wednesday);
-            WriteLine(Days.Wednesday == Days.Sunday);
-
-            string value = Enum.GetName(Days.Wednesday.GetType(), 8);
-            if (value == null)
-            {
-                WriteLine("value is null");
-            }
-            else { 
-                WriteLine("Value: " + value);
-            }
-
-            WriteLine(Enum.IsDefined(Days.Wednesday.GetType(), currentDay));
+            /**
+             * Задание 1
+             * 
+             * Сделать перечисления с рабочими и выходными днями недели.
+             * Написать программу, которая прнимиает день недели через аргументы командной строки и сообщает, 
+             * существует ли такой день недели и, если существует, то рабочий или выходной.
+             * Дополнительная сложность:
+             * 1. Результат работы программы не должен зависеть от исходного регистра символов в аргументах 
+             * командной строки.
+             * 2. Если пользователь не указал день недели в аргументах командной строки, то запрашивать
+             * у пользователя ввод дня недели.
+             * 
+             * Задание 2
+             * 
+             * Написать программу которая принимает от пользователя размеры массива N и M и создает массив 
+             * соответствующего размера заполняя его случайными величинами от -100 до +100. 
+             * 
+             * Надо полученным массивом выполните следующие операции:
+             * 1. Сжать массив, удалив из него все 0 и, заполнить освободившиеся справа элементы значениями –1
+             * 2. Преобразовать массив так, чтобы сначала шли все отрицательные элементы, а потом положительные
+             * (0 считать положительным)
+             * 3. Написать программу, которая предлагает пользователю ввести число и считает, сколько раз это число
+             * встречается в массиве.
+             * 4. В двумерном массиве порядка M на N поменяйте местами заданные столбцы.
+             * 
+             * */
 
             ReadKey();
         }

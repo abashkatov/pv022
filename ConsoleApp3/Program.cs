a648b7ec58bf646dbf19c010e890000edca2d010
abashkatov <as@bashkatov.pro>
2021-01-19 19:50:33 +0300

    Вывод матрицы на основе аргументов командной строки

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            // Принимать через аргументы командной строки два значения: высота матрицы, ширина матрицы.
+            // Задание. Принимать через аргументы командной строки два значения: высота матрицы, ширина матрицы.
             // Выводить матрицу на экран.
             /**
              * Принимать размер матрицы числами
@@ -22,13 +22,53 @@ namespace ConsoleApp3
              *      8  9 10 11
              * 
              * Принимать размер матрицы ключами со значениями
-             * -h=2 -w=3
+             * -h=2000 -w=3
              * 
              * */
 
-            WriteLine(args.Length);
-            foreach (string arg in args) { 
-                WriteLine(arg);
+            int h = 0, w = 0;
+            foreach (string arg in args) 
+            {
+                // Вариант чтения 1
+                //if (arg.StartsWith("-h=")) 
+                //{
+                //    h = int.Parse(arg.Substring(3));
+                //} else if (arg.StartsWith("-w="))
+                //{ 
+                //    w = int.Parse(arg.Substring(3));
+                //}
+
+                // Вариант чтения 2
+                string[] parts = arg.Split('=');
+                switch (parts[0]) {
+                    case "-h":
+                        h = int.Parse(parts[1]);
+                        break;
+                    case "-w":
+                        w = int.Parse(parts[1]);
+                        break;
+                    default:
+                        WriteLine("Error");
+                        return;
+                }
+            }
+            if (h == 0 && w == 0) {
+                WriteLine("Ошибка");
+                return;
+            }
+            WriteLine($"H: {h}");
+            WriteLine($"W: {w}");
+
+            // Вариант вывода 1 - сделан самостоятельно
+
+            // Вариант вывода 2
+            int num = h * w;
+            for (int i = 0; i < num; i++) {
+                Write("{0, 5}", i);
+                if ((i + 1) % w == 0)
+                {
+                    WriteLine();
+                }
             }
 
             ReadKey();

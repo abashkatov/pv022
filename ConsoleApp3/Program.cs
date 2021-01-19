44190e135a2d5c2cc253b4f5b3b2d0904b29abc0
abashkatov <as@bashkatov.pro>
2021-01-19 21:01:45 +0300

    Пример работы с перечислениями

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,72 +4,40 @@ using static System.Console;
 
 namespace ConsoleApp3
 {
+    enum Days:short { Monday = 0, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
+    enum Days1 { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
+    enum Days2 { Monday, Tuesday=5, Wednesday, Thursday=15, Friday, Saturday, Sunday}
     class Program
     {
         static void Main(string[] args)
         {
-            // Задание. Принимать через аргументы командной строки два значения: высота матрицы, ширина матрицы.
-            // Выводить матрицу на экран.
-            /**
-             * Принимать размер матрицы числами
-             * 2 3
-             *      0 1 2
-             *      3 4 5
-             * 
-             * 3 4
-             *      0  1  2  3
-             *      4  5  6  7
-             *      8  9 10 11
-             * 
-             * Принимать размер матрицы ключами со значениями
-             * -h=2000 -w=3
-             * 
-             * */
+            //string currentDay = "Monday";
 
-            int h = 0, w = 0;
-            foreach (string arg in args) 
+            //string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sturday", "Sunday" };
+            //short currentDay = 1;
+
+            Random rnd = new Random();
+            Array enumValues = Enum.GetValues(typeof(Days));
+
+            foreach (Object day in enumValues) {
+                WriteLine((Days)day + " = " + (short)day);
+            }
+
+            string currentDay = "Monday";
+
+            WriteLine(Days.Wednesday + " = " + (short)Days.Wednesday);
+            WriteLine(Days.Wednesday == Days.Sunday);
+
+            string value = Enum.GetName(Days.Wednesday.GetType(), 8);
+            if (value == null)
             {
-                // Вариант чтения 1
-                //if (arg.StartsWith("-h=")) 
-                //{
-                //    h = int.Parse(arg.Substring(3));
-                //} else if (arg.StartsWith("-w="))
-                //{ 
-                //    w = int.Parse(arg.Substring(3));
-                //}
-
-                // Вариант чтения 2
-                string[] parts = arg.Split('=');
-                switch (parts[0]) {
-                    case "-h":
-                        h = int.Parse(parts[1]);
-                        break;
-                    case "-w":
-                        w = int.Parse(parts[1]);
-                        break;
-                    default:
-                        WriteLine("Error");
-                        return;
-                }
+                WriteLine("value is null");
             }
-            if (h == 0 && w == 0) {
-                WriteLine("Ошибка");
-                return;
+            else { 
+                WriteLine("Value: " + value);
             }
-            WriteLine($"H: {h}");
-            WriteLine($"W: {w}");
 
-            // Вариант вывода 1 - сделан самостоятельно
-
-            // Вариант вывода 2
-            int num = h * w;
-            for (int i = 0; i < num; i++) {
-                Write("{0, 5}", i);
-                if ((i + 1) % w == 0)
-                {
-                    WriteLine();
-                }
-            }
+            WriteLine(Enum.IsDefined(Days.Wednesday.GetType(), currentDay));
 
             ReadKey();
         }

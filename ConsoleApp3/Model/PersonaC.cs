8d6f8e60f684d41316b0fad87c6152b1f0afec42
abashkatov <as@bashkatov.pro>
2021-01-21 22:29:46 +0300

    Классы

---
+++ ConsoleApp3/Model/PersonaC.cs
@@ -0,0 +1,13 @@
+
+namespace ConsoleApp3.Model
+{
+    class PersonaC : Person
+    {
+        public PersonaC(): base("sdgsdfg", 1)
+        {
+            System.Console.WriteLine("Конструктор PersonaC()");
+            //this.
+            //CustomClass cc = new CustomClass();
+        }
+    }
+}

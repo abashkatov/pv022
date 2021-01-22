c246cfef30b6d9a073ebdf18f8e1cb5e234b4836
abashkatov <as@bashkatov.pro>
2021-01-22 20:51:25 +0300

    Передача аргументов по ссылке и по значению, Ref, Out

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,36 +1,70 @@
-﻿using ConsoleApp3.Model;
-using static ConsoleApp3.Model.Person;
-using static System.Console;
+﻿using static System.Console;
 
 namespace ConsoleApp3
 {
     class Program
     {
-        /**
-         * private
-         * protected
-         * protected internal
-         * internal
-         * public
-         * */
-        /**
-         * static
-         * const
-         * readonly
-         * */
+        // Перерыв до 20:34
+        #region задание
+        class Foo
+        {
+            public int value = 1;
+        }
+        static int ChangeInt(int n)
+        {
+            n++;
+            return n;
+        }
+        static Foo ChangeFoo(Foo foo)
+        {
+            foo.value++;
+            return foo;
+        }
+        #endregion
+        #region int
+        static void WithValue(int i)
+        {
+            i = 10;
+        }
+        #endregion
+        #region ref int
+        static void WithValueRef(ref int i)
+        {
+            i = 10;
+        }
+        #endregion
+        #region Foo
+        static void WithReference(Foo foo)
+        {
+            foo.value = 10;
+
+            foo = new Foo();
+            foo.value = 200;
+        }
+        #endregion
+        #region ref Foo
+        static void WithReferenceRef(ref Foo foo)
+        {
+            foo.value = 10;
+
+            foo = new Foo();
+        }
+        #endregion
         static void Main(string[] args)
         {
-            Person 
-                person = new Person("bbb")
-            ;
-            char[] chars = { 'g' };
+            int i = 1;
+            Foo foo1 = new Foo(), foo2 = foo1;
 
-            person.SetAge(1);
+            WriteLine(foo1.value); // 1
+            foo1.value++;
+            WriteLine(foo2.value); // 2
 
-            person.SetName("sdfgsdg");
-            person.SetName(chars);
+            //WithValue(i);
+            //WithValueRef(ref i);
+            WithReferenceRef(ref foo1);
 
-            WriteLine(person.GetName());
+            WriteLine(foo1.value); // 1
+            WriteLine(foo2.value); // 10
 
             ReadKey();
         }

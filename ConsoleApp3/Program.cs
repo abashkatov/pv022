f5c01206dc2cfb70abfe74313868628a3551c840
abashkatov <as@bashkatov.pro>
2021-01-26 20:07:44 +0300

    Наследование

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,69 +2,86 @@
 
 namespace ConsoleApp3
 {
+    abstract class Person { 
+        public string Name;
+        public int Age;
+        public bool hasAccess()
+        {
+            return false;
+        }
+        abstract public int GetNumber();
+        public string GetSelf() {
+            return "Person";
+        }
+        abstract public string GetThis();
+        abstract public string GetBase();
+    }
+    class Employer: Person {
+        public string Position;
+        public new bool hasAccess() 
+        {
+            return true;
+        }
+        public override int GetNumber()
+        {
+            return 1;
+        }
+        public new string GetSelf()
+        {
+            return "Employer";
+        }
+        public override string GetThis()
+        {
+            return this.GetSelf();
+        }
+        public override string GetBase()
+        {
+            return base.GetSelf();
+        }
+    }
+    sealed class Client: Person {
+        public int CardNumber;
+
+        public override int GetNumber()
+        {
+            return 2;
+        }
+        public new string GetSelf()
+        {
+            return "Client";
+        }
+        public override string GetThis()
+        {
+            return this.GetSelf();
+        }
+        public override string GetBase()
+        {
+            return base.GetSelf();
+        }
+    }
+
     class Program
     {
-        // Перерыв до 20:34
-        #region задание
-        class Foo
-        {
-            public int value = 1;
-        }
-        static int ChangeInt(int n)
-        {
-            n++;
-            return n;
-        }
-        static Foo ChangeFoo(Foo foo)
-        {
-            foo.value++;
-            return foo;
-        }
-        #endregion
-        #region int
-        static void WithValue(int i)
-        {
-            i = 10;
-        }
-        #endregion
-        #region ref int
-        static void WithValueRef(ref int i)
-        {
-            i = 10;
-        }
-        #endregion
-        #region Foo
-        static void WithReference(Foo foo)
-        {
-            foo.value = 10;
-
-            foo = new Foo();
-            foo.value = 200;
-        }
-        #endregion
-        #region ref Foo
-        static void WithReferenceRef(ref Foo foo)
-        {
-            foo.value = 10;
-
-            foo = new Foo();
-        }
-        #endregion
         static void Main(string[] args)
         {
-            int i = 1;
-            Foo foo1 = new Foo(), foo2 = foo1;
+            //object o = new Client();
+            Client client = new Client();
+            Person client2 = new Client();
+            WriteLine(client.GetSelf());
+            WriteLine(client2.GetSelf());
 
-            WriteLine(foo1.value); // 1
-            foo1.value++;
-            WriteLine(foo2.value); // 2
+            if (client is Person)
+            {
+                WriteLine((client as Person).GetSelf());
+                WriteLine(((Person)client).GetSelf());
+            }
 
-            //WithValue(i);
-            //WithValueRef(ref i);
-            WithReferenceRef(ref foo1);
+            Person[] persons = { client, new Employer() };
 
-            WriteLine(foo1.value); // 1
-            WriteLine(foo2.value); // 10
+            foreach (Person person in persons) {
+                person.hasAccess();
+                string name = person.Name;
+            }
 
             ReadKey();
         }

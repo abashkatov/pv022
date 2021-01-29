5f857d2379994813226c6973042235b5fe4b0036
abashkatov <as@bashkatov.pro>
2021-01-29 21:24:58 +0300

    Интерфейс ICloneable

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,17 +4,7 @@ using static System.Console;
 
 namespace ConsoleApp3
 {
-    class DayComparator : IComparer
-    {
-        public int Compare(object x, object y)
-        {
-            int a = (x as Day).Number;
-            int b = (y as Day).Number;
-
-            return a == b ? 0 : (a > b ? 1 : -1);
-        }
-    }
-    class Day
+    class Day: ICloneable
     {
         public Day(int number)
         {
@@ -22,21 +12,19 @@ namespace ConsoleApp3
         }
 
         public int Number { get; set; }
+
+        public object Clone()
+        {
+            return new Day(Number);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Day[] days = { 
-                new Day(2000000000),
-                new Day(2),
-                new Day(5),
-                new Day(-2000000000),
-            };
-            Array.Sort(days, new DayComparator());
-            foreach (Day day in days) {
-                WriteLine(day.Number);
-            }
+            Day day = new Day(1),
+                day2 = day.Clone() as Day;
+
             ReadKey();
         }
     }

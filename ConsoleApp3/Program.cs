972921aa51f2dc2d85c1fb34046f69a20c8e7961
abashkatov <as@bashkatov.pro>
2021-02-02 21:37:55 +0300

    checked и unchecked арифметические операции

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,62 +4,23 @@ using static System.Console;
 
 namespace ConsoleApp3
 {
-
-    class A
-    {
-        public bool isAllow(string currentAge, string allowAge)
-        {
-            int nCurrentAge, nAllowAge;
-
-            try
-            {
-                nCurrentAge = int.Parse(currentAge);
-            }
-            catch (Exception e) {
-                ArgumentException exception = new ArgumentException(e.Message, nameof(currentAge), e);
-                throw exception;
-            }
-
-            nAllowAge = int.Parse(allowAge);
-
-            //try { } catch (Exception e) { }
-            //ArgumentException exception = new ArgumentException(e.Message, "", e);
-            //throw exception;
-            // "currentAge"
-            // nameof(currentAge)
-
-            return nCurrentAge >= nAllowAge;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            A a = new A();
-            string currentAge, allowAge;
-            bool result = false;
-            do
+            byte n = byte.MaxValue - 1;
+            WriteLine(n); // 1  0 0 0 0  0 0 0 0
+            n++;
+            WriteLine(n);
+            unchecked
             {
-                Write("Введите ваш возраст: ");
-                currentAge = ReadLine();
-                Write("Введите возраст, с которого разрешены операции: ");
-                allowAge = ReadLine();
-                try
-                {
-                    result = a.isAllow(currentAge, allowAge);
-                    if (!result)
-                    {
-                        WriteLine("Операция запрещена");
-                    }
-                }
-                catch (ArgumentException e) {
-                    WriteLine($"Error input argument {e.ParamName} is missing:" + e.Message);
-                }
+                n++;
+                n--;
+            }
+            WriteLine(n);
+            byte r = unchecked(++n);
 
-
-                WriteLine();
-            } while (!result);
-            WriteLine("Операция РАЗРЕШЕНА");
+            WriteLine(r);
 
             ReadKey();
         }

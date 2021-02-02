20e42ddd15a6f1f8adec3ababeaa622a81cd1b97
abashkatov <as@bashkatov.pro>
2021-02-02 20:02:25 +0300

    Блок finally

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,42 +4,25 @@ using static System.Console;
 
 namespace ConsoleApp3
 {
-    class A {
-        // Исключения:
-        //   T:System.ArgumentNullException:
-        //     Свойство s имеет значение null.
-        //
-        //   T:System.FormatException:
-        //     s имеет неправильный формат.
-        //
-        //   T:System.OverflowException:
-        //     s представляет число, которое меньше значения System.Int32.MinValue или больше
-        //     значения System.Int32.MaxValue.
-        public int First(string s) {
+    class A
+    {
+        public int First(string s)
+        {
             int age;
             try
             {
-                age = Second(s);
-            }
-            catch (ArgumentException e)
-            {
-                throw new Exception("Our custom exception", e);
-            }
-
-            return age;
-        }
-        public int Second(string s) {
-            int age = short.Parse(s);
-
-            return age;
-        }
-        private int Parse(string s) {
-            int age;
-            if (int.TryParse(s, out age)) {
+                age = short.Parse(s);
+                WriteLine("End of try");
                 return age;
             }
+            catch (Exception e) {
 
-            throw new ArgumentException("Не удалось спарсить строку");
+                throw;
+            }
+            finally {
+                WriteLine("finally");
+                age = 15;
+            }
         }
     }
     class Program
@@ -57,16 +40,8 @@ namespace ConsoleApp3
                     input = null;
                 }
 
-                try
-                {
-                    age = a.First(input);
-                    WriteLine($"Ваш возраст {age}");
-                }
-                catch (Exception e)
-                {
-                    throw new Exception("Our custom exception", e);
-                }
-
+                age = a.First(input);
+                WriteLine($"Ваш возраст {age}");
 
                 WriteLine();
             }

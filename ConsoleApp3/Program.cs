6acf063972c768700467c849604796661091a16b
abashkatov <as@bashkatov.pro>
2021-02-04 19:52:21 +0300

    Finalize

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,46 +4,45 @@ using static System.Console;
 
 namespace ConsoleApp3
 {
+    class Resource
+    {
+        string Name;
+        string s = "1";
+
+        public Resource(string name)
+        {
+            Name = name;
+        }
+
+        ~Resource()
+        {
+            WriteLine(Name + " has been finalized");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            /**
-             *  Поколения
-             * 0 - 256 Кб
-             * 1 - 2 Мб
-             * 2 - 10 Мб
-             * */
+            WriteLine("Start");
+            /*
+            1. Выделение памяти
+            2. Задание состояния объекта
+            3. Использование объекта
+            ---- Сборщик мусора ----
+            4. Уничтожение состояния
+            5. Освобождение памяти
+            */
 
-            string s1 = new string('*', 100);
-            WriteLine(GC.MaxGeneration);
-            WriteLine("{0: ### ### ### ###}", GC.GetTotalMemory(false));
-            WriteLine("Generation is " + GC.GetGeneration(s1));
-            //ArrayList ar = new ArrayList();
-            //for (int i = 0; i < 10; i++)
-            //{
-            //ar.Add(FillMemory(1024));
-            //}
-            //FillMemory2(64);
-            //FillMemory2(64);
+            Resource r = new Resource("O1");
+            CreateResource();
+
             GC.Collect();
-
-            //WriteLine("{0: ### ### ### ###}", GC.GetTotalMemory(true));
-            WriteLine("Generation is " + GC.GetGeneration(s1));
-            GC.Collect();
-            WriteLine("Generation is " + GC.GetGeneration(s1));
-
-            //GC.Collect();
-            //WriteLine(GC.GetTotalMemory(false));
 
             ReadKey();
         }
-
-        static void FillMemory2(int size) {
-            string s = new string('*', 342*size);
-        }
-        static string FillMemory(int size) {
-            return new string('*', 342*size);
+        public static void CreateResource()
+        {
+            Resource r = new Resource("O2");
         }
     }
 }

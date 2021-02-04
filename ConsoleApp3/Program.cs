a62a9dd40c8e27b12e17440e53eed55c8687dbf5
abashkatov <as@bashkatov.pro>
2021-02-04 19:26:14 +0300

    Сборщик мусора и поколения памяти

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,21 +8,42 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            byte n = byte.MaxValue - 1;
-            WriteLine(n); // 1  0 0 0 0  0 0 0 0
-            n++;
-            WriteLine(n);
-            unchecked
-            {
-                n++;
-                n--;
-            }
-            WriteLine(n);
-            byte r = unchecked(++n);
+            /**
+             *  Поколения
+             * 0 - 256 Кб
+             * 1 - 2 Мб
+             * 2 - 10 Мб
+             * */
 
-            WriteLine(r);
+            string s1 = new string('*', 100);
+            WriteLine(GC.MaxGeneration);
+            WriteLine("{0: ### ### ### ###}", GC.GetTotalMemory(false));
+            WriteLine("Generation is " + GC.GetGeneration(s1));
+            //ArrayList ar = new ArrayList();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //ar.Add(FillMemory(1024));
+            //}
+            //FillMemory2(64);
+            //FillMemory2(64);
+            GC.Collect();
+
+            //WriteLine("{0: ### ### ### ###}", GC.GetTotalMemory(true));
+            WriteLine("Generation is " + GC.GetGeneration(s1));
+            GC.Collect();
+            WriteLine("Generation is " + GC.GetGeneration(s1));
+
+            //GC.Collect();
+            //WriteLine(GC.GetTotalMemory(false));
 
             ReadKey();
+        }
+
+        static void FillMemory2(int size) {
+            string s = new string('*', 342*size);
+        }
+        static string FillMemory(int size) {
+            return new string('*', 342*size);
         }
     }
 }

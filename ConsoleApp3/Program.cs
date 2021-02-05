e1279eeb6ada301ee137ccbb96dfd31fc8de1c0d
abashkatov <as@bashkatov.pro>
2021-02-05 19:28:41 +0300

    SortedList

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -5,51 +5,34 @@ using static System.Console;
 
 namespace ConsoleApp3
 {
+    class Comparator : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            if ((int)y == (int)x)
+            {
+                return 0;
+            }
+
+            return (int)y > (int)x ? 1 : -1;
+        }
+            // 2000_000_000 + 2000_000_000
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var collection = new ArrayList(40);
-            WriteLine("Count " + collection.Count);
-            WriteLine("Capacity " + collection.Capacity);
-            WriteLine();
+            var collection = new SortedList(new Comparator());
+            collection.Add(-2_000_000_015, 5);
+            collection.Add("dfg", "sdgdfg");
+            collection.Add(new Comparator(), new Comparator());
+            collection.Add(2000_000_001, 5);
+            foreach (DictionaryEntry pair in collection) {
+                WriteLine(pair.Key);
+            }
+            //var stack = new Stack(); // LIFO = Last In First Out
+            //var queue = new Queue(); // FIFO = First In First Out
 
-            collection.Add("1");
-            WriteLine("Count " + collection.Count);
-            WriteLine("Capacity " + collection.Capacity);
-            WriteLine();
-
-            collection.Add("1");
-            collection.Add("1");
-            collection.Add("1");
-            collection.Add("1");
-            WriteLine("Count " + collection.Count);
-            WriteLine("Capacity " + collection.Capacity);
-            WriteLine();
-
-            collection.Add("1");
-            collection.Add("1");
-            collection.Add("1");
-            collection.Add("1");
-            WriteLine("Count " + collection.Count);
-            WriteLine("Capacity " + collection.Capacity);
-            WriteLine();
-
-            collection.Add("1");
-            collection.Add("1");
-            collection.Add("1");
-            collection.Add("1");
-            collection.Add("1");
-            collection.Add("1");
-            collection.Add("1");
-            collection.Add("1");
-            WriteLine("Count " + collection.Count);
-            WriteLine("Capacity " + collection.Capacity);
-            WriteLine();
-
-
-            WriteLine(collection[0]);
-            
 
             ReadKey();
         }

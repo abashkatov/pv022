c085cc3b1fe1db533e7c547066f78b23f078b3e1
abashkatov <as@bashkatov.pro>
2021-02-05 19:55:00 +0300

    Упаковка и распаковка

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using static System.Console;
@@ -9,23 +10,13 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            var collection = new List<string>();
-            collection.Add("sdfgdg");
-            WriteLine(collection[0]);
+            object o;
+            int i = 5;
 
-            List<string> rC;
-            var registredCollection = Register<List<string>, int, int, int, int>(collection);
-            registredCollection = Register<List<string>, string, int, int, int>(collection);
-            if (registredCollection is List<string>) {
-                rC = registredCollection as List<string>;
-            }
+            o = i;
+            i = (int)o;
 
             ReadKey();
-        }
-
-        static T Register<T, R, S, A, Vbf>(T item) {
-            R t;
-            return item;
         }
     }
 }

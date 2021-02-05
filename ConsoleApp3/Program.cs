8c40ce3d69cef9b9eb6c797486b4c2d8e097c1a8
abashkatov <as@bashkatov.pro>
2021-02-05 19:46:01 +0300

    Обобщение методов

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,40 +1,31 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using static System.Console;
 
 namespace ConsoleApp3
 {
-    class Comparator : IComparer
-    {
-        public int Compare(object x, object y)
-        {
-            if ((int)y == (int)x)
-            {
-                return 0;
-            }
-
-            return (int)y > (int)x ? 1 : -1;
-        }
-            // 2000_000_000 + 2000_000_000
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var collection = new SortedList(new Comparator());
-            collection.Add(-2_000_000_015, 5);
-            collection.Add("dfg", "sdgdfg");
-            collection.Add(new Comparator(), new Comparator());
-            collection.Add(2000_000_001, 5);
-            foreach (DictionaryEntry pair in collection) {
-                WriteLine(pair.Key);
-            }
-            //var stack = new Stack(); // LIFO = Last In First Out
-            //var queue = new Queue(); // FIFO = First In First Out
+            var collection = new List<string>();
+            collection.Add("sdfgdg");
+            WriteLine(collection[0]);
 
+            List<string> rC;
+            var registredCollection = Register<List<string>, int, int, int, int>(collection);
+            registredCollection = Register<List<string>, string, int, int, int>(collection);
+            if (registredCollection is List<string>) {
+                rC = registredCollection as List<string>;
+            }
 
             ReadKey();
+        }
+
+        static T Register<T, R, S, A, Vbf>(T item) {
+            R t;
+            return item;
         }
     }
 }

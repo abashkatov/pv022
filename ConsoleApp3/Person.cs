979ec506065d0f2ef02b17a67d78bc98b36dc13a
abashkatov <as@bashkatov.pro>
2021-02-11 19:04:19 +0300

    LINQ с массивом int

---
+++ ConsoleApp3/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public int Height;
+
+        public Person()
+        { }
+        public Person(string name, int age, int height)
+        {
+            Name = name;
+            Age = age;
+            Height = height;
+        }
+    }
+}
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -6,60 +7,22 @@ using static System.Console;
 
 namespace ConsoleApp3
 {
-
-    /**
-     * v 1. Добавить делегат
-     * v 2. Добавить событие
-     * v 3. Добавить метод для подписки или использовать существующий
-     * v 4. Подписаться методом (из п.3) на событие (из п. 2)
-     * 5. При изменении имени вызывать событие
-     * */
     public class Program
     {
-        delegate void ActionWithPerson(Person person);
-        class Person {
-            public event ActionWithPerson NameChanged;
-            private string _name;
-            public string Name {
-                get => _name;
-                set {
-                    _name = value;
-                    NameChanged?.Invoke(this);
-                }
-            }
-        }
-        class Form
-        {
-            public void Draw(Person person)
-            {
-                Clear();
-                CursorLeft = 0;
-                CursorTop = 0;
-                WriteLine("Имя: " + person.Name);
-            }
-            public string AskNewName()
-            {
-                CursorLeft = 0;
-                CursorTop = 3;
-                Write("Введите новое имя: ");
-                return ReadLine();
-            }
-        }
         static void Main(string[] args)
         {
-            Person person = new Person();
-            Form form = new Form();
+            IEnumerable<int> query;
+            int[] intRange = { 11, 23, 1, 2};
 
-            person.NameChanged += form.Draw;
+            query = from i in intRange
+                    where i % 2 == 1
+                    orderby i
+                    select i;
 
-            string name;
-            form.Draw(person);
-            do
-            {
-                name = form.AskNewName();
-                person.Name = name;
-                //form.Draw(person);
-            } while (true);
+            foreach (int i in query) {
+                WriteLine(i);
+            }
+
             ReadKey();
         }
     }

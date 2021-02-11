c68570843fbf38946f054fa6fcd0c1c12197a115
abashkatov <as@bashkatov.pro>
2021-02-11 20:58:03 +0300

    FileStream

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,6 +4,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using static System.Console;
+using System.IO;
+using System.Text;
 
 namespace ConsoleApp3
 {
@@ -11,32 +13,23 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            IEnumerable<IGrouping<bool, Person>> groups;
-            Person[] persons = { 
-                new Person("Name 1", 15, 170),
-                new Person("Name 2", 17, 180),
-                new Person("Name 3", 20, 190),
-                new Person("Name 4", 22, 200),
-                new Person("Name 5", 16, 160),
-            };
+            string source = "Hello World!";
+            string dest;
 
-            groups = from person in persons
-                         //where person.Age > 18
-                         //orderby person.Age
-                     group person by person.Age > 18 into gr
-                     where gr.Count() > 1
-                     select gr
-                     ;
-                    //select person;
-
-
-            foreach (IGrouping<bool, Person> group in groups) {
-                string title = group.Key ? "adults" : "children";
-                WriteLine("===" + title + "===");
-                foreach (Person person in group) {
-                    WriteLine(person.Name);
-                }
+            using (FileStream fs = new FileStream("example.txt", FileMode.OpenOrCreate))
+            {
+                byte[] buffer = Encoding.Default.GetBytes(source);
+                fs.Write(buffer, 0, buffer.Length);
             }
+
+            using (FileStream fs = new FileStream("example.txt", FileMode.Open))
+            {
+                byte[] buffer = new byte[fs.Length];
+                int i = fs.Read(buffer, 0, (int)fs.Length);
+                WriteLine("Прочитано " + i + " байт");
+                dest = Encoding.Default.GetString(buffer);
+            }
+            WriteLine(dest);
 
             ReadKey();
         }

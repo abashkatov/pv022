cae62bfe4ce693f8aea9ebe0a996ccd3eda2e442
abashkatov <as@bashkatov.pro>
2021-02-12 21:41:38 +0300

    Регулярные выражения

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,18 +9,40 @@ using System.Text;
 using System.Xml.Serialization;
 using System.Text.Json;
 using System.Xml;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp3
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.test(1);
+            string 
+                url = "cian.ru/spb/nevskiy/11/dfhhfghejfhsdhfshdkjfhsdfgsdfgsdfg",
+                email1 = "df-g@dfh.ru",
+                email2 = "df.g@dfh"
+            ;
+
+            string
+                emailPattern = @"^\w+@\w+\.\w+$",
+                urlPattern = @"^cian\.ru\/(\w+)\/(\w+)\/(\w+)\/"
+            ;
+
+            Regex 
+                regexMail = new Regex(emailPattern),
+                regexUrl = new Regex(urlPattern)
+                ;
+            WriteLine(email1 + " is match " + regexMail.IsMatch(email1));
+            WriteLine(email2 + " is match " + regexMail.IsMatch(email2));
+
+            var matches = regexUrl.Matches(url);
+            foreach (Match match in matches) {
+                foreach (Group group in match.Groups)
+                {
+                    WriteLine(group.Value);
+                }
+            }
+
             ReadKey();
         }
     }

9629ef0442381af7be04f347636fb0a47da7322e
abashkatov <as@bashkatov.pro>
2021-02-16 21:00:33 +0300

    Описаны задачи

---
+++ ConsoleApp3/Entity/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp3.Entity
+{
+    [Serializable]
+    public class Person
+    {
+        private static int count = 0;
+
+        public readonly int ID;
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+            ID = ++count;
+        }
+        public Person(string name, int age): this()
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
